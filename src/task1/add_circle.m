d04da00dfdfbb121b050001f827766d6372b31f2
Thomas Lee <lisihan969@gmail.com>
2015-07-26 20:56:28 +0800

    Refine implement of add_circle

--- src/task1/add_circle.m
+++ src/task1/add_circle.m
@@ -6,12 +6,10 @@ function circled_img = add_circle(img)
     r = min(x_max, y_max) / 2;
     center = [(x_max + 1) / 2, (y_max + 1) / 2];
 
-    for k = 1:3
-        for x = 1:x_max
-            for y = 1:y_max
-                if norm([x y] - center) <= r
-                    circled_img(x, y, :) = [255 0 0];
-                end
+    for x = 1:x_max
+        for y = 1:y_max
+            if norm([x y] - center) <= r
+                circled_img(x, y, :) = [255 0 0];
             end
         end
     end
