4d99458f4ef2d8c9be80f4bea91b428e7f2d21cf
Thomas Lee <lisihan969@gmail.com>
2015-07-29 00:19:03 +0800

    Try to read from the whole image when the header's missing

--- src/task3/read_pixel_domain.m
+++ src/task3/read_pixel_domain.m
@@ -5,11 +5,15 @@ function data_str = read_pixel_domain(data_img)
     % Read the first 32 bit for data_len.
     data_len = (2 .^ [0:31]) * double(bitget(data_img(32:-1:1), 1)');
 
-    % Read body.
     code_len = data_len * 8 + 32;
     if code_len > numel(data_img)  % Wrong header.
-        error 'No data detected in the image.'
+        warning(['Wrong header detected in the image, ' ...
+                 'trying to read from the whole image.']);
+        data_len = ceil((numel(data_img) - 32) / 8);
+        code_len = data_len * 8 + 32;
     end
+
+    % Read body.
     body = reshape(bitget(data_img(33:code_len), 1), [8, data_len]);
 
     data_str = char([128 64 32 16 8 4 2 1] * double(body));
