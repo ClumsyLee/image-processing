3d8b7f0f19f12441f402e3b70f8534edf0d12653
Thomas Lee <lisihan969@gmail.com>
2015-07-28 12:50:05 +0800

    Add huffman_decode

---
+++ src/task2/huffman_decode.m
@@ -0,0 +1,21 @@
+%% huffman_decode: Decode huffman code
+function [index, len] = huffman_decode(codes, huffman_table)
+    candidate = 1:size(huffman_table, 1);
+
+    len = 0;
+    while length(candidate) > 1
+        len = len + 1;
+        for k = 1:length(candidate)
+            row = candidate(k);
+            if codes(len) ~= huffman_table(row, len)
+                candidate(k) = 0;  % Mark as unqualified.
+            end
+        end
+        candidate(candidate == 0) = [];  % Eliminate unqualified.
+    end
+
+    if candidate
+        index = candidate;
+    else
+        error(['Unknown code: ' int2str(codes(1:len))]);
+    end
