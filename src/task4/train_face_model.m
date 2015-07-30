61b116e22242979eb391af91f8cbb70aac06ff78
Thomas Lee <lisihan969@gmail.com>
2015-07-30 18:49:53 +0800

    Implement train_face_model

---
+++ src/task4/train_face_model.m
@@ -0,0 +1,11 @@
+%% train_face_model: Train a face model using face images
+%% L is the bits used for every color component
+function v = train_face_model(imgs, L)
+    bin_num = 2 ^ (3 * L);
+    v = zeros(bin_num, 1);
+    pixels = 0;
+    for k = 1:length(imgs)
+        img = quantize_img(imgs{k}, L);
+        v = v + histc(img(:), 0:bin_num-1) / numel(img);
+    end
+end
