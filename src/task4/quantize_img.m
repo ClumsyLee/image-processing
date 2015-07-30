5fc79d3f29503091648fdde05e180418cdcae142
Thomas Lee <lisihan969@gmail.com>
2015-07-30 19:06:14 +0800

    Use a MUCH faster way to implement quantize_img

--- src/task4/quantize_img.m
+++ src/task4/quantize_img.m
@@ -1,14 +1,7 @@
 %% quantize_img: Quantize image
 function quantized_img = quantize_img(img, L)
-    row = size(img, 1);
-    col = size(img, 2);
-    quantized_img = zeros(row, col);
-    for row = 1:row
-        for col = 1:col
-            pixel = img(row, col, :);
-            bits = [bitget(pixel(3), 9-L:8), ...
-                    bitget(pixel(2), 9-L:8), ...
-                    bitget(pixel(1), 9-L:8)];
-            quantized_img(row, col) = bi2de(uint32(bits));
-        end
-    end
+    shift = 8 - L;
+    quantized_img = uint32(bitshift(img, -shift));
+    quantized_img = bitshift(quantized_img(:, :, 1), 2 * L) + ...
+                    bitshift(quantized_img(:, :, 2),     L) + ...
+                             quantized_img(:, :, 3);
