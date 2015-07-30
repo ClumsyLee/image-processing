1369b414c0872b26d79fa172d8f0e1f2d31250b6
Thomas Lee <lisihan969@gmail.com>
2015-07-30 23:23:24 +0800

    Use faces_UL and faces_LR as args

--- src/task4/highlight_face.m
+++ src/task4/highlight_face.m
@@ -1,22 +1,20 @@
 %% highlight_face: Highlight faces in an image
-function varargout = highlight_face(img, faces)
+function varargout = highlight_face(img, faces_UL, faces_LR)
     WIDTH = 2;
 
     r = img(:, :, 1);
     g = img(:, :, 2);
     b = img(:, :, 3);
+    row = (1:size(img, 1))';
+    col = 1:size(img, 2);
 
-    for k = 1:size(faces, 3)
-        face = faces(:, :, k);
-        row = (1:size(img, 1))';
-        col = 1:size(img, 2);
-
-        row_range = (row >= face(1, 1) & row <= face(1, 2));
-        col_range = (col >= face(2, 1) & col <= face(2, 2));
-        row_bound = row_range & (row - face(1, 1) < WIDTH | ...
-                                 face(1, 2) - row < WIDTH);
-        col_bound = col_range & (col - face(2, 1) < WIDTH | ...
-                                 face(2, 2) - col < WIDTH);
+    for k = 1:size(faces_UL, 2)
+        row_range = (row >= faces_UL(1, k) & row <= faces_LR(1, k));
+        col_range = (col >= faces_UL(2, k) & col <= faces_LR(2, k));
+        row_bound = row_range & (row - faces_UL(1, k) < WIDTH | ...
+                                 faces_LR(1, k) - row < WIDTH);
+        col_bound = col_range & (col - faces_UL(2, k) < WIDTH | ...
+                                 faces_LR(2, k) - col < WIDTH);
 
         frame = bsxfun(@and, row_bound, col_range) | ...
                 bsxfun(@and, col_bound, row_range);
